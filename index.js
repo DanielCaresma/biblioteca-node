a9644ec7c1b222996b640bddd417ab30e6ac19e5
Daniel Caresma Silva <91802662+DanielCaresma@users.noreply.github.com>
2023-03-29 14:31:06 -0300

    Criando função para pegar arquivo

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('olá mundo');
+import fs from 'fs'
+import chalk from 'chalk';
+
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'Arquivo não encontrado!'))
+}
+
+async function pegaArquivo (caminhoDoArquivo) {
+    try {
+        const encoding = 'utf-8'
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+        console.log(chalk.green(texto))
+    } catch (erro) {
+        trataErro(erro)
+    }
+}
+
+pegaArquivo('./arquivos/texto.md')
+pegaArquivo('./arquivos/')
