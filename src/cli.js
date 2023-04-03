01b3b354052c0e740d4335ece31465080829bc41
Daniel Caresma Silva <91802662+DanielCaresma@users.noreply.github.com>
2023-04-03 13:26:45 -0300

    Criando validação dos links

--- src/cli.js
+++ src/cli.js
@@ -1,19 +1,32 @@
 import chalk from "chalk"
 import fs from "fs"
 import pegaArquivo from "./index.js"
+import listaValidada from "./http-validacao.js"
+
+
 
 const caminho = process.argv
 
-function imprimeLista (resultado, arquivo = '') {
-    console.log(
-        chalk.yellow('Lista de links'),
-        chalk.black.bgGreen(arquivo),
-        resultado
-    )
+async function imprimeLista (valida, resultado, arquivo = '') {
+
+    if (valida) {
+        console.log(
+            chalk.yellow('Lista validada'),
+            chalk.black.bgGreen(arquivo),
+            await listaValidada(resultado)
+        )
+    } else {
+        console.log(
+            chalk.yellow('Lista de links'),
+            chalk.black.bgGreen(arquivo),
+            resultado
+        )
+    } 
 }
 
 async function processaTexto(argumentos) {
     const caminho = argumentos[2]
+    const valida = argumentos[3] === '--valida'
 
     try {
         fs.lstatSync(caminho)
@@ -27,14 +40,14 @@ async function processaTexto(argumentos) {
     if(fs.lstatSync(caminho).isFile()) {
         const resultado = await pegaArquivo(caminho)
 
-        imprimeLista(resultado)
+        imprimeLista(valida, resultado)
     } else if(fs.lstatSync(caminho).isDirectory()) {
         const arquivos = await fs.promises.readdir(caminho)
 
         arquivos.forEach(async (nomeDeArquivo) => {
             const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
             
-            imprimeLista(lista, nomeDeArquivo)
+            imprimeLista(valida, lista, nomeDeArquivo)
         })
     }
 }
